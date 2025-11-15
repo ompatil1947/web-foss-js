9fd1db3bfef42111221725ff28e7905cb274f56a
ompatil1947 <omspatil1947@gmail.com>
2025-11-15 19:53:29 +0530

    file added

---
+++ LCS2025016/stylr.js
@@ -0,0 +1,13 @@
+console.log("hello om")
+let varA="hello om";
+const CONSTVAL=23;
+let str1 = "Hello";
+let str2 = 'World';
+let str3 = `Template literal with ${str1}`;
+console.error('error hai bhai');
+console.warn("warning ahead");
+function changecolor() {
+  let element = document.querySelector("#om");  
+  element.style.backgroundColor="blue"
+
+}
